899ea37ef101aba81d55c263e98ba42b425ec912
gizmore <gizmore@wechall.net>
2022-09-08 01:05:22 +0200

    '7.0.1-r1581 debugging and fixes'

--- Method/GetSecret.php
+++ Method/GetSecret.php
@@ -1,18 +1,18 @@
 <?php
 namespace GDO\Websocket\Method;
 
-use GDO\Util\Common;
-use GDO\Websocket\Module_Websocket;
 use GDO\Core\GDT_JSON;
-use GDO\Core\Module_Core;
+use GDO\Core\GDT_UInt;
 use GDO\Core\MethodAjax;
+use GDO\Core\Module_Core;
 use GDO\Session\GDO_Session;
+use GDO\Websocket\Module_Websocket;
 
 /**
  * Get cookie and user JSON for external apps.
  * 
  * @author gizmore
- * @version 6.11.1
+ * @version 7.0.1
  * @since 4.0.0
  */
 final class GetSecret extends MethodAjax
@@ -22,13 +22,20 @@ final class GetSecret extends MethodAjax
 		return t('btn_connect');
 	}
 	
+	public function gdoParameters() : array
+	{
+		return [
+			GDT_UInt::make('count')->min(0)->max(3)->notNull()->initial('0'),
+		];
+	}
+	
 	public function execute()
 	{
 		$json = [
 			'user' => Module_Core::instance()->gdoUserJSON(),
 			'cookie' => GDO_Session::$COOKIE_NAME,
 			'secret' => Module_Websocket::instance()->secret(),
-			'count' => Common::getRequestInt('count', 0),
+			'count' => $this->gdoParameterValue('count'),
 		];
 		return GDT_JSON::make()->value($json);
 	}
