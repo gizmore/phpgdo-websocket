4f493d39de8ab0464b583c6ab17e94883dc220cc
gizmore <gizmore@wechall.net>
2022-09-07 10:37:59 +0200

    '7.0.1-r1576 fix'

--- Server/GWS_CommandForm.php
+++ Server/GWS_CommandForm.php
@@ -16,45 +16,56 @@ use GDO\Core\GDT_JSON;
 
 /**
  * Call MethodForm via websockets.
+ *
  * @author gizmore
  * @version 6.10.1
  * @since 5.0
  */
 abstract class GWS_CommandForm extends GWS_Command
 {
+
 	/**
+	 *
 	 * @return MethodForm
 	 */
 	public abstract function getMethod();
-	
-	public function fillRequestVars(GWS_Message $msg) {}
-	
+
+	public function fillRequestVars(GWS_Message $msg)
+	{
+	}
+
 	public function execute(GWS_Message $msg)
 	{
-	    parent::execute($msg);
-	    
-	    $method = $this->getMethod();
-		
-	    $this->fillRequestVars($msg);
-		
+		parent::execute($msg);
+
+		$method = $this->getMethod();
+
+		$this->fillRequestVars($msg);
+
+		$form = $method->getForm();
+		$this->removeCSRF($form);
+		// $this->removeCaptcha($form);
+
 		try
 		{
 			$form = GWS_Form::bindMethodForm($method, $msg);
 		}
 		catch (GDO_Exception $ex)
 		{
-			$msg->replyErrorMessage($msg->cmd(), t("err_bind_form", [$ex->getMessage()]));
+			$msg->replyErrorMessage($msg->cmd(), t("err_bind_form", [
+				$ex->getMessage()
+			]));
 			return;
 		}
 
 		$this->selectSubmit($form);
-		$this->removeCSRF($form);
-// 		$this->removeCaptcha($form);
 		$button = $method->getAutoButton();
-		$response = $method->executeWithInputs([$button => '1']);
+		$response = $method->executeWithInputs([
+			$button => '1'
+		]);
 		$this->postExecute($msg, $form, $response);
 	}
-	
+
 	public function postExecute(GWS_Message $msg, GDT_Form $form, GDT_Response $response)
 	{
 		if ($response->hasError())
@@ -69,17 +80,16 @@ abstract class GWS_CommandForm extends GWS_Command
 			$this->afterReplySuccess($msg);
 		}
 	}
-	
+
 	public function afterReplySuccess(GWS_Message $msg)
 	{
-		
 	}
-	
+
 	public function replySuccess(GWS_Message $msg, GDT_Form $form, GDT_Response $response)
 	{
 		$msg->replyBinary($msg->cmd(), $this->payloadFromResponse($response));
 	}
-	
+
 	private function payloadFromResponse(GDT_Response $response)
 	{
 		$payload = '';
@@ -87,15 +97,15 @@ abstract class GWS_CommandForm extends GWS_Command
 		{
 			$payload .= $this->payloadFromField($gdt);
 		}
-		
+
 		if (@Website::$TOP_RESPONSE)
 		{
-		    $payload .= Website::$TOP_RESPONSE->renderCLI() . chr(0);
+			$payload .= Website::$TOP_RESPONSE->renderCLI() . chr(0);
 		}
-		
+
 		return $payload;
 	}
-	
+
 	private function payloadFromField(GDT $gdt)
 	{
 		$payload = '';
@@ -116,61 +126,55 @@ abstract class GWS_CommandForm extends GWS_Command
 		}
 		elseif ($gdt instanceof GDT_Success)
 		{
-		    $payload .= GWS_Message::wrS($gdt->renderText());
+			$payload .= GWS_Message::wrS($gdt->renderText());
 		}
-		
-// 		if ($fields = $gdt->getFields())
-// 		{
-// 			foreach ($fields as $gdt2)
-// 			{
-// 				$payload .= $this->payloadFromField($gdt2);
-// 			}
-// 		}
-		
+
+		// if ($fields = $gdt->getFields())
+		// {
+		// foreach ($fields as $gdt2)
+		// {
+		// $payload .= $this->payloadFromField($gdt2);
+		// }
+		// }
+
 		return $payload;
 	}
 
 	/**
+	 *
 	 * @param GDT_Form $form
 	 * @return GDT_Submit[]
 	 */
 	protected function getSubmits(GDT_Form $form)
 	{
 		$submits = [];
-		foreach ($form->getAllFields() as $field)
+		foreach ($form->actions() as $field)
 		{
-		    if ($field instanceof GDT_Submit)
-		    {
-		        $submits[] = $field;
-		    }
+			if ($field instanceof GDT_Submit)
+			{
+				$submits[] = $field;
+			}
 		}
-// 		foreach ($form->actions()->getFieldsRec() as $field)
-// 		{
-// 		    if ($field instanceof GDT_Submit)
-// 		    {
-// 		        $submits[] = $field;
-// 		    }
-// 		}
 		return $submits;
 	}
-	
+
 	protected function removeCaptcha(GDT_Form $form)
 	{
 		$form->removeFieldNamed('captcha');
 	}
-	
+
 	protected function removeCSRF(GDT_Form $form)
 	{
 		$form->removeFieldNamed('xsrf');
 	}
-	
+
 	protected function selectSubmit(GDT_Form $form)
 	{
 		$this->selectSubmitNum($form, 0);
 	}
-	
+
 	protected function selectSubmitNum(GDT_Form $form, $num)
-	{	
+	{
 		$submits = $this->getSubmits($form);
 		if ($submit = @$submits[$num])
 		{
