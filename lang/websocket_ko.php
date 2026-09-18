e968064800827134de5a21b2771ba4df52f86a0a
gizmore <gizmore@wechall.net>
2026-09-18 04:07:09 +0200

    'more korean'

---
+++ lang/websocket_ko.php
@@ -0,0 +1,17 @@
+<?php
+return [
+	'btn_connect' => '연결하다',
+	'link_ws_exec' => '웹소켓 클라이언트',
+	'err_gws_unknown_cmd' => '알 수 없는 Websocket 명령: %s.',
+	'err_setting_validate' => '설정을 저장할 수 없습니다: %s',
+	'err_setting_unchanged' => '설정이 변경되지 않았습니다.',
+	'err_bind_form' => 'Websocket 입력을 양식에 바인딩하지 못했습니다: %s',
+	'err_gws_dup_code' => '다음 클래스는 기존 ws 코드를 사용합니다: %s.',
+	'form_error_title' => '양식 오류',
+	'cfg_ws_autoconnect' => '자동으로 연결하시겠습니까?',
+	'cfg_ws_guests' => '게스트 연결을 허용하시겠습니까?',
+	'cfg_ws_port' => '웹소켓 포트',
+	'cfg_ws_timer' => 'Weboscket 타이머 간격',
+	'cfg_ws_processor' => 'Websocket PHP 프로세서 바이너리',
+	'cfg_ws_url' => 'Websocket 프런트엔드 연결 URL',
+];
